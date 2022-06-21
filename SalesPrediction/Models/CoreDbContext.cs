084341a0541ebfd2a11538a9d272d284dc2b3880
Trai Torti <trai4.t@gmail.com>
2022-06-21 12:53:29 +1000

    Sales Prediction Init

---
+++ SalesPrediction/Models/CoreDbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SalesPrediction.Models
+{
+    public partial class CoreDbContext : DbContext
+    {
+        public CoreDbContext()
+        {
+        }
+
+        public CoreDbContext(DbContextOptions<CoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblSales> TblSales { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-9MT5TES;Database=SalesHistoryDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TblSales>(entity =>
+            {
+                entity.HasKey(e => e.SalesId)
+                    .HasName("PK__Sales__C952FB123BED40E5");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
